6673816e5fc7934ad6925453140e7c5ac1ab70b3
Sreedhar Joshi <joshi.sreedhar@gmail.com>
2018-05-30 19:26:41 -0700

    clean unused imports'

--- CodingService/src/main/java/com/enterprise/codingService/CodingController.java
+++ CodingService/src/main/java/com/enterprise/codingService/CodingController.java
@@ -4,22 +4,15 @@ package com.enterprise.codingService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.stereotype.Component;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @RestController
-@Component
-@Configuration
-@EnableConfigurationProperties
-@ConfigurationProperties(prefix = "hello")
 public class CodingController{
     private static Logger log = LoggerFactory.getLogger(CodingController.class);
 
@@ -28,7 +21,7 @@ public class CodingController{
 
     @GetMapping(value="users/{id}/repos")
     @ResponseBody public List<String> getRepositories(@PathVariable(value="id") String id){
-        log.error("called with " + id  + configuration.getWorld()[]);
+        log.error("called with " + id  + configuration.getWorld());
         return new ArrayList<>();
     }
 }
